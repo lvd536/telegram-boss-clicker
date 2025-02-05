2ee1a1cad1d5d84ca3143b79cfb8cb1573842030
lvd536 <nbikiti@gmail.com>
2025-02-05 16:47:15 +0400

    Add new boss names, fix var warnings

--- Game/Clicker/Boss/Boss.cs
+++ Game/Clicker/Boss/Boss.cs
@@ -18,26 +18,47 @@ public static class Boss
                 if (plly is not null && (plly.Boss == null || plly.Boss.Health <= 0 || string.IsNullOrEmpty(plly.Boss.Name)))
                 {
                     Console.WriteLine("Create Boss");
-                    string[] _bossNames = { "Marshal", "Hellsing", "Harry", "Hunter", "Killa" };
-                    string _bossName = _bossNames[rnd.Next(_bossNames.Length)];
-                    int _bossLvl = Math.Max(1, rnd.Next(plly.Level, plly.Level + 3));
-                    int _bossHealth = Math.Max(10, rnd.Next(_bossLvl * 5, _bossLvl * 25));
-                    double _bossExp = Math.Max(5, rnd.Next((_bossLvl * _bossHealth) / rnd.Next(2, 5), (_bossLvl * _bossHealth) * 2));
-                    long _bossMoney = Math.Max(10, rnd.Next(_bossLvl * _bossHealth, (_bossLvl * _bossHealth) * rnd.Next(2, 10)));
-                    long _bossCashiers = rnd.Next(0, 5);
+                    string[] _bossNames = { 
+                        "Marshal", 
+                        "Hellsing", 
+                        "Harry", 
+                        "Hunter", 
+                        "Killa", 
+                        "Vanguard", 
+                        "Shadowfang", 
+                        "Ironclad", 
+                        "Frostbite", 
+                        "Blackbeard", 
+                        "Warlord", 
+                        "Dreadnought", 
+                        "Spectre", 
+                        "Ragnarok", 
+                        "Nightshade", 
+                        "Titan", 
+                        "Blaze", 
+                        "Phantom", 
+                        "Overlord", 
+                        "Apex"
+                    };
+                    string bossName = _bossNames[rnd.Next(_bossNames.Length)];
+                    int bossLvl = Math.Max(1, rnd.Next(plly.Level, plly.Level + 3));
+                    int bossHealth = Math.Max(10, rnd.Next(bossLvl * 5, bossLvl * 25));
+                    double bossExp = Math.Max(5, rnd.Next((bossLvl * bossHealth) / rnd.Next(2, 5), (bossLvl * bossHealth) * 2));
+                    long bossMoney = Math.Max(10, rnd.Next(bossLvl * bossHealth, (bossLvl * bossHealth) * rnd.Next(2, 10)));
+                    long bossCashiers = rnd.Next(0, 5);
 
                     plly.Boss = new Database.Boss
                     {
-                        Name = _bossName,
-                        Level = _bossLvl,
-                        Health = _bossHealth,
-                        Experience = _bossExp,
-                        Money = _bossMoney,
-                        Cashiers = _bossCashiers
+                        Name = bossName,
+                        Level = bossLvl,
+                        Health = bossHealth,
+                        Experience = bossExp,
+                        Money = bossMoney,
+                        Cashiers = bossCashiers
                     };
                 
                     await db.SaveChangesAsync();
-                    Console.WriteLine($"Boss created: {_bossName}, HP: {_bossHealth}");
+                    Console.WriteLine($"Boss created: {bossName}, HP: {bossHealth}");
                 }
             }
             catch (Exception ex)
